d7d7ca675f5695e84189228efcf1ae846d195738
sultan.amanbai <ysembaev_s@bankrbk.kz>
2023-06-26 23:05:27 +0600

    Add payments and integrations apps. Developing booking app and APIs. Onevision integration for payments

--- apps/bookings/views.py
+++ apps/bookings/views.py
@@ -30,4 +30,3 @@ class BookingHistoryView(JSONRendererMixin, ListAPIView):
 
     def get_queryset(self):
         return Booking.objects.filter(club_user__user=self.request.user).order_by('-created_at')
-d
