6f66a860ca5c6d72376c02b8c6bb91f829ca8846
ingeniousalpha <usembaevsultan08@gmail.com>
2024-03-07 12:29:03 +0500

    Remove redundant plus sign

--- telegram_auth/src/app.controller.ts
+++ telegram_auth/src/app.controller.ts
@@ -48,23 +48,23 @@ export class TelegramService {
         const phoneNumber = cleanPhoneNumber(message.contact.phone_number);
         const founded = await this.telegramRepository.findOne({
           where: {
-            mobile_phone: "+" + phoneNumber,
+            mobile_phone: phoneNumber,
           },
         });
         if (founded) {
           await this.telegramRepository.update(
             {
-              mobile_phone: "+" + phoneNumber,
+              mobile_phone: phoneNumber,
             },
             {
               chat_id: message.chat.id,
-              mobile_phone: "+" + phoneNumber,
+              mobile_phone: phoneNumber,
             },
           );
         } else {
           await this.telegramRepository.insert({
             chat_id: message.chat.id,
-            mobile_phone: "+" + phoneNumber,
+            mobile_phone: phoneNumber,
           });
         }
 
