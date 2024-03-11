818b6b7e08a5d1b04bce7c027daac2994dfdf729
ingeniousalpha <usembaevsultan08@gmail.com>
2024-03-11 21:28:48 +0500

    Fix 3

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -51,7 +51,7 @@ class SigninV2View(PublicJSONRendererMixin, CreateAPIView):
 
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
 
 
 class VerifyOTPV2View(PublicJSONRendererMixin, GenericAPIView):
