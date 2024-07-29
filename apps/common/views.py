55fb6ef25da96324705727fdc1b74cea2bd43215
ingeniousalpha <usembaevsultan08@gmail.com>
2024-07-29 16:36:18 +0500

    Upgrade dashboard 2

--- apps/common/views.py
+++ apps/common/views.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-from django.db.models import Sum, Count, Q, F, CharField, Value
+from django.db.models import Sum, Count, Q, F, Func
 from django.db.models.functions import Concat
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView, GenericAPIView
@@ -19,20 +19,21 @@ from ..users.models import User
 
 from urllib.parse import unquote
 
-MONTHS = {
-    1: "Январь",
-    2: "Февраль",
-    3: "Март",
-    4: "Апрель",
-    5: "Май",
-    6: "Июнь",
-    7: "Июль",
-    8: "Август",
-    9: "Сентябрь",
-    10: "Октябрь",
-    11: "Ноябрь",
-    12: "Декабрь"
+MONTHS_NAMES = {
+    "1": "Январь",
+    "2": "Февраль",
+    "3": "Март",
+    "4": "Апрель",
+    "5": "Май",
+    "6": "Июнь",
+    "7": "Июль",
+    "8": "Август",
+    "9": "Сентябрь",
+    "10": "Октябрь",
+    "11": "Ноябрь",
+    "12": "Декабрь"
 }
+
 
 class DocumentListView(PublicJSONRendererMixin, ListAPIView):
     queryset = Document.objects.all()
@@ -103,6 +104,16 @@ class LobbyAppVersionsView(PublicJSONRendererMixin, GenericAPIView):
         })
 
 
+class MonthYear(Func):
+    function = 'CONCAT'
+    template = "%(function)s(EXTRACT(MONTH FROM %(expressions)s), '.', EXTRACT(YEAR FROM %(expressions)s))"
+
+
+def get_month_str(month_dot_year):
+    month, year = month_dot_year.split('.')
+    return f"{MONTHS_NAMES.get(month)} {year}"
+
+
 def dashboard_view(request):
     period = request.GET.get('period')  # today/last_week/last_month/last_year
     group_by_field = 'created_at__date'
@@ -111,51 +122,58 @@ def dashboard_view(request):
     elif period == 'last_week':
         filter_period = timezone.now() - timezone.timedelta(days=7)
     elif period == 'last_year':
-        group_by_field = 'created_at__month'
+        group_by_field = 'month_year'
         filter_period = timezone.now() - timezone.timedelta(days=365)
     else:
         period = 'last_month'
         filter_period = timezone.now() - timezone.timedelta(days=30)
 
-    queryset = Booking.objects.filter(created_at__gte=filter_period)
-    successful_bookings = queryset.filter(
+    bookings_total = Booking.objects.filter(created_at__gte=filter_period)
+    users_total = User.objects.filter(created_at__gte=filter_period)
+    users_count_total = users_total.count()
+    successful_bookings = bookings_total.filter(
         Q(payments__status=PaymentStatuses.PAYMENT_APPROVED) | Q(use_cashback=True)
     ).filter(created_at__gte=filter_period)
     cancelled_bookings = successful_bookings.filter(is_cancelled=True)
-    bookings_total_count = queryset.count()
+    bookings_total_count = bookings_total.count()
     bookings_successful_count = successful_bookings.count()
     bookings_cancelled_count = cancelled_bookings.count()
     bookings_successful_revenue_amount = successful_bookings.exclude(is_cancelled=True).aggregate(
         Sum('total_amount')
     )['total_amount__sum'] or 0
-    users_total_count = User.objects.all()
 
-    dates = queryset.values(group_by_field).annotate(bookings_count=Count('id')).order_by(group_by_field)
+    if period == 'last_year':
+        dates = bookings_total.annotate(month_year=MonthYear('created_at')).values(group_by_field).annotate(
+            bookings_count=Count('id')).order_by(group_by_field)
+        payments_dates = Payment.objects.filter(
+            status=PaymentStatuses.PAYMENT_APPROVED, created_at__gte=filter_period
+        ).annotate(month_year=MonthYear('created_at')).values(group_by_field).annotate(
+            payments_count=Count('id')).order_by(group_by_field)
+        users_dates = users_total.annotate(month_year=MonthYear('created_at')).values(group_by_field).annotate(
+            users_count=Count('id')).order_by(group_by_field)
+    else:
+        dates = bookings_total.values(group_by_field).annotate(bookings_count=Count('id')).order_by(group_by_field)
+        payments_dates = Payment.objects.filter(
+            status=PaymentStatuses.PAYMENT_APPROVED, created_at__gte=filter_period
+        ).values(group_by_field).annotate(payments_count=Count('id')).order_by(group_by_field)
+        users_dates = users_total.values(group_by_field).annotate(users_count=Count('id')).order_by(group_by_field)
+
     bookings_dates_list = []
     bookings_count_list = []
     for item in dates:
-        bookings_dates_list.append(MONTHS.get(item[group_by_field]) if period=="last_year" else item[group_by_field])
+        bookings_dates_list.append(get_month_str(item[group_by_field]) if period=="last_year" else item[group_by_field])
         bookings_count_list.append(item['bookings_count'])
-        
-    successful_payments = Payment.objects.filter(status=PaymentStatuses.PAYMENT_APPROVED)
-    payments_dates = successful_payments.filter(
-        created_at__gte=filter_period
-    ).values(group_by_field).annotate(payments_count=Count('id')).order_by(group_by_field)
+
     payments_dates_list = []
     payments_count_list = []
     for item in payments_dates:
-        payments_dates_list.append(MONTHS.get(item[group_by_field]) if period=="last_year" else item[group_by_field])
+        payments_dates_list.append(get_month_str(item[group_by_field]) if period=="last_year" else item[group_by_field])
         payments_count_list.append(item['payments_count'])
 
-    all_users = User.objects.all()
-    users_count_total = all_users.count()
-    users_dates = all_users.filter(
-        created_at__gte=filter_period
-    ).values(group_by_field).annotate(users_count=Count('id')).order_by(group_by_field)
     users_dates_list = []
     users_count_list = []
     for item in users_dates:
-        users_dates_list.append(MONTHS.get(item[group_by_field]) if period=="last_year" else item[group_by_field])
+        users_dates_list.append(get_month_str(item[group_by_field]) if period=="last_year" else item[group_by_field])
         users_count_list.append(item['users_count'])
 
     bookings_summary_table = (Booking.objects
