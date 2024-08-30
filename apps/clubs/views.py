4b41ebd042b27c73b92b49054d306b4354979a33
ingeniousalpha <usembaevsultan08@gmail.com>
2024-08-30 17:15:59 +0500

    Refactor logic of Time packets: consider timezone

--- apps/clubs/views.py
+++ apps/clubs/views.py
@@ -60,7 +60,7 @@ class ClubBranchTimePacketListView(JSONRendererMixin, ListAPIView):
     def get_queryset(self):
         return super().get_queryset().filter(
             club_computer_group_id=self.kwargs.get('hall_id'),
-            is_active=True, available_days__number=timezone.now().weekday() + 1,
+            is_active=True, available_days__number=timezone.now().astimezone().weekday() + 1,
         ).filter(
             (Q(available_time_start__lte=timezone.now().astimezone().time()) & Q(
                 available_time_end__gte=timezone.now().astimezone().time())) |
